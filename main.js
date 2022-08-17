58b9196c47a1076460d2e8d005df68b3ea5a04ce
Bertrand <blaurancin@gmail.com>
2022-08-17 14:09:58 +0200

    refactor: become const and functions

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 console.log("start");
 
-var samples = 10;
+const samples = 10;
 console.log("samples: ", samples);
-var r = 1;
-var cx0 = 50;
-var cy0 = 50;
-var r0 = 45;
+const r = 1;
+const cx0 = 50;
+const cy0 = 50;
+const r0 = 45;
+const svgns = "http://www.w3.org/2000/svg";
+const container = document.querySelector("g.samples");
 
-for (var i = 0; i < samples; i++) {
-  var svgns = "http://www.w3.org/2000/svg";
-  var angle = (2 * Math.PI * i) / samples;
-  var x = cx0 + Math.cos(angle) * r0;
-  var y = cy0 + Math.sin(angle) * r0;
+function getAngleFromIndex(index, samples) {
+  return (2 * Math.PI * index) / samples;
+}
 
-  var container = document.querySelector("g.samples");
-  var circle = document.createElementNS(svgns, "circle");
+function getPointFromAngle(angle) {
+  const x = cx0 + Math.cos(angle) * r0;
+  const y = cy0 + Math.sin(angle) * r0;
+  return { x, y };
+}
+
+for (let i = 0; i < samples; i++) {
+  //
+  const angle = getAngleFromIndex(i, samples);
+
+  const { y, x } = getPointFromAngle(angle);
+
+  const circle = document.createElementNS(svgns, "circle");
   circle.setAttributeNS(null, "cx", x);
   circle.setAttributeNS(null, "cy", y);
   circle.setAttributeNS(null, "r", r);
