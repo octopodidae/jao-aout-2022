a8c508ad499ee405fb816d6dac7c00aad2493c2a
Bertrand <blaurancin@gmail.com>
2022-08-17 15:13:08 +0200

    refactor: eS3

---
+++ Board.js
@@ -0,0 +1,41 @@
+const r = 1;
+const cx0 = 50;
+const cy0 = 50;
+const r0 = 45;
+const svgns = "http://www.w3.org/2000/svg";
+const container = document.querySelector("g.samples");
+
+const getPointFromAngle = (angle) => {
+  const x = cx0 + Math.cos(angle) * r0;
+  const y = cy0 + Math.sin(angle) * r0;
+  return { x, y };
+};
+
+const getAngleFromIndex = (index, samples) => {
+  return (2 * Math.PI * index) / samples;
+};
+
+function Board() {
+  console.log("instanciating board");
+  this.config = { samples: 23, multiplicationFactor: 34 };
+  this.name = "Board";
+}
+
+Board.prototype.draw = function () {
+  console.log("I am drawing");
+  for (let i = 0; i < this.config.samples; i++) {
+    const angle = getAngleFromIndex(i, this.config.samples);
+
+    const { x, y } = getPointFromAngle(angle);
+
+    const circle = document.createElementNS(svgns, "circle");
+    circle.setAttributeNS(null, "cx", x);
+    circle.setAttributeNS(null, "cy", y);
+    circle.setAttributeNS(null, "r", r);
+    container.appendChild(circle);
+  }
+};
+
+Board.prototype.setConfig = function (config) {
+  this.config = config;
+};
--- main.js
+++ main.js
@@ -1,33 +1,8 @@
-console.log("start");
-
-const samples = 10;
-console.log("samples: ", samples);
-const r = 1;
-const cx0 = 50;
-const cy0 = 50;
-const r0 = 45;
-const svgns = "http://www.w3.org/2000/svg";
-const container = document.querySelector("g.samples");
-
-const getPointFromAngle = (angle) => {
-  const x = cx0 + Math.cos(angle) * r0;
-  const y = cy0 + Math.sin(angle) * r0;
-  return { x, y };
-};
-
-const getAngleFromIndex = (index, samples) => {
-  return (2 * Math.PI * index) / samples;
-};
-
-for (let i = 0; i < samples; i++) {
-  //
-  const angle = getAngleFromIndex(i, samples);
-
-  const { y, x } = getPointFromAngle(angle);
-
-  const circle = document.createElementNS(svgns, "circle");
-  circle.setAttributeNS(null, "cx", x);
-  circle.setAttributeNS(null, "cy", y);
-  circle.setAttributeNS(null, "r", r);
-  container.appendChild(circle);
+try {
+  console.log("start");
+  const board = new Board();
+  board.setConfig({ samples: 10, multiplicationFactor: 2 });
+  board.draw();
+} catch (err) {
+  console.log("err: ", err);
 }
