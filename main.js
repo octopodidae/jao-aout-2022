c3be2f66493c7868154dcf53e812228870192c52
Bertrand <blaurancin@gmail.com>
2022-08-17 11:48:05 +0200

    dev: small circles are on the big circle

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ console.log("start");
 
 var samples = 10;
 console.log("samples: ", samples);
+var r = 1;
+var cx0 = 50;
+var cy0 = 50;
+var r0 = 45;
 
 for (var i = 0; i < samples; i++) {
   var svgns = "http://www.w3.org/2000/svg";
-  var x = 34;
-  var y = 56;
+  var angle = (2 * Math.PI * i) / samples;
+  var x = cx0 + Math.cos(angle) * r0;
+  var y = cy0 + Math.sin(angle) * r0;
+
   var container = document.querySelector("g.samples");
   var circle = document.createElementNS(svgns, "circle");
   circle.setAttributeNS(null, "cx", x);
   circle.setAttributeNS(null, "cy", y);
-  circle.setAttributeNS(null, "r", 1);
+  circle.setAttributeNS(null, "r", r);
   container.appendChild(circle);
 }
