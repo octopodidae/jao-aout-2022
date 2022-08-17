95ae5ff560d2626544d097f26437584a905efffe
Bertrand <blaurancin@gmail.com>
2022-08-17 11:38:57 +0200

    dev: retrieve and insert inside doc

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log("start");
+
+var samples = 10;
+console.log("samples: ", samples);
+
+for (var i = 0; i < samples; i++) {
+  var svgns = "http://www.w3.org/2000/svg";
+  var x = 34;
+  var y = 56;
+  var container = document.querySelector("g.samples");
+  var circle = document.createElementNS(svgns, "circle");
+  circle.setAttributeNS(null, "cx", x);
+  circle.setAttributeNS(null, "cy", y);
+  circle.setAttributeNS(null, "r", 1);
+  container.appendChild(circle);
+}
