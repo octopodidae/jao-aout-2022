aafc1907a1bcf6e8592be01e0dde2c831e3b3286
Bertrand <blaurancin@gmail.com>
2022-08-18 16:44:56 +0200

    dev: added mode in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,26 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./src/main.ts",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
+module.exports = (_, args) => {
+  const mode = args.mode ?? "development";
+  return {
+    entry: "./src/main.ts",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    mode,
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+  };
 };
