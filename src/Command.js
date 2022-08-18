3237fa7bc9d2157f9a5f7d1f9151d981e5d2a1d9
Bertrand <blaurancin@gmail.com>
2022-08-18 14:07:19 +0200

    dev: added typescript

--- src/Command.js
+++ src/Command.js
@@ -9,11 +9,10 @@ export class Command {
   }
   applyConfig() {
     const arr = Object.keys(this.config);
-    console.log("arr: ", arr);
 
     for (const key of arr) {
       const slider = document.querySelector(`div.command label.${key} input`);
-      console.log("slider: ", slider);
+
       slider.value = this.config[key];
       const label = document.querySelector(`div.command label.${key} span`);
       label.innerHTML = this.config[key];
@@ -21,15 +20,14 @@ export class Command {
   }
   initActions() {
     const arr = Object.keys(this.config);
-    console.log("arr: ", arr);
 
     for (const key of arr) {
       const slider = document.querySelector(`div.command label.${key} input`);
-      console.log("slider: ", slider);
+
       slider.addEventListener("input", (event) => {
-        // console.log("event: ", event);
+        //
         const value = slider.value;
-        console.log("value: ", value);
+
         this.config[key] = value;
         this.applyConfig();
         this.callback(this.config);
