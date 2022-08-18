c712dfc01d32f0583e936df322e23b747cbec97e
Bertrand <blaurancin@gmail.com>
2022-08-18 09:45:07 +0200

    dev: draw lines

--- Board.js
+++ Board.js
@@ -4,6 +4,7 @@ const cy0 = 50;
 const r0 = 45;
 const svgns = "http://www.w3.org/2000/svg";
 const container = document.querySelector("g.samples");
+const container2 = document.querySelector("g.lines");
 
 const getPointFromAngle = (angle) => {
   const x = cx0 + Math.cos(angle) * r0;
@@ -15,14 +16,22 @@ const getAngleFromIndex = (index, samples) => {
   return (2 * Math.PI * index) / samples;
 };
 
+const drawLine = (p1, p2) => {
+  const line = document.createElementNS(svgns, "line");
+  line.setAttributeNS(null, "x1", p1.x);
+  line.setAttributeNS(null, "y1", p1.y);
+  line.setAttributeNS(null, "x2", p2.x);
+  line.setAttributeNS(null, "y2", p2.y);
+  container2.appendChild(line);
+};
+
 export class Board {
   constructor() {
     console.log("instanciating board");
     this.config = { samples: 23, multiplicationFactor: 34 };
-    this.name = "Board";
   }
-  draw() {
-    console.log("I am drawing");
+  drawCircles() {
+    console.log("I am drawing circle");
     for (let i = 0; i < this.config.samples; i++) {
       const angle = getAngleFromIndex(i, this.config.samples);
 
@@ -34,6 +43,30 @@ export class Board {
       circle.setAttributeNS(null, "r", r);
       container.appendChild(circle);
     }
+  }
+
+  drawLines() {
+    console.log("I am drawing line");
+    for (let i = 0; i < this.config.samples; i++) {
+      this.drawLine(i);
+    }
+  }
+
+  drawLine(index) {
+    const angle1 = getAngleFromIndex(index, this.config.samples);
+    console.log("angle1: ", angle1);
+    const angle2 = getAngleFromIndex(
+      index * this.config.multiplicationFactor,
+      this.config.samples
+    );
+    const pt1 = getPointFromAngle(angle1);
+    const pt2 = getPointFromAngle(angle2);
+    drawLine(pt1, pt2);
+  }
+
+  draw() {
+    this.drawCircles();
+    this.drawLines();
   }
 
   setConfig(config) {
